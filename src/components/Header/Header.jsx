ae7dcd8154fa1ec1bbc0aa3fc09ff92ab6cbb01a
Roman Shell <rshelegeda@ukr.net>
2023-07-21 12:52:17 +0300

    new

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Button from "../Button/Button";
+
 import { useTelegram } from '../../hooks/useTelegram';
+import Button from 'react-bootstrap/esm/Button';
 
 
 const Header = () => {
+    
     const {tg, user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
             HEADER
+            <Button onClick={tg.close()}></Button>
         </div>
     );
 }
